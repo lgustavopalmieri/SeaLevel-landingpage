7488b366d35193f6c1dd6eaf2a8d80dc7e04d19d
lgustavopalmieri <lgustavopalmieri@gmail.com>
2021-10-17 12:33:54 -0300

    update 2

--- script.js
+++ script.js
@@ -5,4 +5,25 @@ var activate = () => {
     mobileMenu.classList.toggle('active')
 } 
 
+    
+
 hamburguer.addEventListener("click",activate)
+
+function aliens(){
+    const aliensBorn = document.querySelector('#aliens-born')
+    
+    
+
+    setInterval(() => {
+        let number = Math.floor(Math.random()*10)
+        let numbers = Math.floor(Math.random()*10)
+        aliensBorn.innerHTML = '1564898321754'+number + '124357623' + numbers
+        
+    },1000)
+    
+    
+}
+
+aliens()
+
+
